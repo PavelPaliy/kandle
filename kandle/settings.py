c64395865a8c35bd892836f4465374d04f96e89a
PavelPaliy <34663443+PavelPaliy@users.noreply.github.com>
2019-06-06 01:59:38 +0300

    commit

--- kandle/settings.py
+++ kandle/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'kandle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'app',
+        'USER': 'sample_user',
+        'PASSWORD': 'sample_password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
